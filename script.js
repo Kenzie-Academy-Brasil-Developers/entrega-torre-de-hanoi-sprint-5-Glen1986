162f1889bad0c7f64006cf90039393bcc36c6605
wallacesfo <wallacesfos@gmail.com>
2021-08-02 19:38:59 -0300

    Feito a movimentação de jogo

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const content = document.querySelector('.conteudo');
 
 // TOWERS
-const tower1 = document.createElement('div');
+const tower1 = document.createElement('label');
 tower1.id = 'stack1';
 tower1.classList.add('wrapper', 'tower');
 content.appendChild(tower1)
+tower1.addEventListener('click', () => verificarIdClique('stack1'));
+tower1.setAttribute('for', 'box1')
 
-const tower2 = document.createElement('div');
+
+const tower2 = document.createElement('label');
 tower2.id = 'stack2';
 tower2.classList.add('wrapper', 'tower');
 content.appendChild(tower2)
+tower2.addEventListener('click', () => verificarIdClique('stack2'));
+tower2.setAttribute('for', 'box2')
 
-const tower3 = document.createElement('div');
+const tower3 = document.createElement('label');
 tower3.id = 'stack3';
 tower3.classList.add('wrapper', 'tower');
 content.appendChild(tower3)
+tower3.addEventListener('click', () => verificarIdClique('stack3'));
+tower3.setAttribute('for', 'box3')
+
 
 //DISCOS
 const disc1 = document.createElement('div');
@@ -42,4 +50,90 @@ disc5.id = 'box-5';
 disc5.classList.add('box', 'box5');
 tower1.appendChild(disc5);
 
+
+/* game */
+
+let primeiroClique;
+let segundoClique;
+
+function verificarIdClique(str){
+    game(str)
+}
+
+function game(str){
+    let selecao = document.getElementById(str);
+
+    if(primeiroClique == undefined){
+        primeiroClique = str; 
+        selecao.style.border = "2px solid black"
+    }else{
+        segundoClique = str;
+        selecao.style.border = "2px solid black"
+    }
+
+
+    if(segundoClique !== null){
+        let pai = document.getElementById(primeiroClique)
+        let ultimoFilho = pai.lastChild;
+
+        //verificação se ele esta clicando em um campo com elementos
+        if(ultimoFilho == null){
+            primeiroClique = undefined
+            segundoClique = undefined
+            tower1.style.border = "1px dotted rgb(63, 104, 138)"
+            tower2.style.border = "1px dotted rgb(63, 104, 138)"
+            tower3.style.border = "1px dotted rgb(63, 104, 138)"
+            alert('você deve selecionar um campo com elementos!')
+        }
+
+
+        //movendo o clique para o campo selecionado
+        twoClick = document.getElementById(segundoClique)
+        twoClick.appendChild(ultimoFilho)
+
+        //resetando as bordas e variaveis para o próximo movimento
+        primeiroClique = undefined
+        segundoClique = undefined
+        tower1.style.border = "1px dotted rgb(63, 104, 138)"
+        tower2.style.border = "1px dotted rgb(63, 104, 138)"
+        tower3.style.border = "1px dotted rgb(63, 104, 138)"
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function verificar(str){
+    ultimoFilho(str)
+};
+
+function largura(x){
+    let select = getElementById(x);
+    
+}
+
+function ultimoFilho(x){
+    let pai = document.getElementById(x)
+    let ultimoFilho = pai.lastChild;
+    mover(ultimoFilho)
+    //console.log(ultimoFilho)
+}
+
+function mover(mov){
+    console.log(mov)
+}*/
 
